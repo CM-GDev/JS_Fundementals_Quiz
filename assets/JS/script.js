55357bf837f58e6192e97bc8d7260662780efe0d
Cristobal Marquez-Glynn <cristobalmqz@gmail.com>
2022-03-16 14:42:13 -0700

    Added function to ask the first question and provide answer options

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -3,14 +3,22 @@ var timerNode = document.querySelector("#timer");
 var mainContNode = document.querySelector("#mainContent");
 var feedbackNode = document.querySelector("#feedback")
 
-// Creating button and br\ elements in DOM
+// Creating button elements in DOM
 var ansOptButton1 = document.createElement ("button");
 var ansOptButton2 = document.createElement ("button");
 var ansOptButton3 = document.createElement ("button");
 var ansOptButton4 = document.createElement ("button");
 
 
+var chosenQustnObj = "";
+var chosenQustn = "";
+var chosenAns1 = "";
+var chosenAns2 = "";
+var chosenAns3 = "";
+var chosenAns4 = "";
 
+var winCounter = 0;
+var loseCounter = 0;
 var isWin = false;
 var timer;
 var timerCount;
@@ -19,41 +27,41 @@ var timerCount;
 var question1 = {
     question: "Which of the following is the assignment operator?",
     choice1: ["=","full"],
-    choice2: "+",
-    choice3: "+=",
-    choice4: "<",
+    choice2: ["+", "ful"],
+    choice3: ["+=", "ful"],
+    choice4: ["<", "ful"],
 };
 
 var question2 = {
     question: "What is the correct syntax for initiating a variable?",
-    choice1: "var pets: 3",
-    choice2: "Pets, 3",
-    choice3: "pets: 3",
+    choice1: ["var pets: 3", "ful"],
+    choice2: ["Pets, 3", "ful"],
+    choice3: ["pets: 3", "ful"],
     choice4: ["var pets = 3", "full"],
 };
 
 var question3 = {
     question: "Which of the following is not a primitive data type?",
     choice1: ["defined","full"],
-    choice2: "bigint",
-    choice3: "number",
-    choice4: "symbol",
+    choice2: ["bigint", "ful"],
+    choice3: ["number", "ful"],
+    choice4: ["symbol", "ful"],
 };
 
 var question4 = {
     question: "What does the '%' operator perform?",
-    choice1: "Returns the quotient between two numbers",
+    choice1: ["Returns the quotient between two numbers", "ful"],
     choice2: ["Returns the remainder between two number","full"],
-    choice3: "Calculates the percentage of number",
-    choice4: "Used to make comments on a JS file",
+    choice3: ["Calculates the percentage of number", "ful"],
+    choice4: ["Used to make comments on a JS file", "ful"],
 };
 
 var question5 = {
     question: "What is the correct syntax for pushing information/data to the console?",
-    choice1: "console.push()",
-    choice2: "console()",
+    choice1: ["console.push()", "ful"],
+    choice2: ["console()", "ful"],
     choice3: ["console.log()","full"],
-    choice4: "log()",
+    choice4: ["log()", "ful"],
 };
 
 // All questions stored into an array for easy access
@@ -93,22 +101,43 @@ function startTimer() {
 
 //Function for appending buttons for each question option. This goes in the .midPanel. 
 function renderQuestBoard () {
-    mainContNode.textContent = " ";
+    
+    mainContNode.children[0].textContent = "";
     feedbackNode.textContent = " ";
     
-    mainContNode.setAttribute("style", "display: block;")
-
+    
     mainContNode.appendChild(ansOptButton1);
     mainContNode.appendChild(ansOptButton2);
     mainContNode.appendChild(ansOptButton3);
     mainContNode.appendChild(ansOptButton4);
 
+    mainContNode.setAttribute("style", "display: block");
+
+    mainContNode.children[1].setAttribute("style", "display: block; block-size: fit-content");
+    mainContNode.children[2].setAttribute("style", "display: block;");
+    mainContNode.children[3].setAttribute("style", "display: block;");
+    mainContNode.children[4].setAttribute("style", "display: block;");
     renderQuestions();
 };
 
 // Function for populating questions and answer options.
 function renderQuestions () {
-    
+    chosenQustnObj = gameQuestions[Math.floor(Math.random() * gameQuestions.length)];
+
+    chosenQustn = chosenQustnObj.question;
+    chosenAns1 = chosenQustnObj.choice1[0];
+    chosenAns2 = chosenQustnObj.choice2[0];
+    chosenAns3 = chosenQustnObj.choice3[0];
+    chosenAns4 = chosenQustnObj.choice4[0];
+
+    mainContNode.children[0].textContent = chosenQustn;
+    ansOptButton1.textContent = chosenAns1;
+    ansOptButton2.textContent = chosenAns2;
+    ansOptButton3.textContent = chosenAns3;
+    ansOptButton4.textContent = chosenAns4;
+
+
+
 }
 
 startButton.addEventListener("click", startGame);
