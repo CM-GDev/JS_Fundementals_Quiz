35947948484961501daa444d35e7576761305464
Cristobal Marquez-Glynn <cristobalmqz@gmail.com>
2022-03-16 13:31:33 -0700

    Added function to clear some panels and render blank board game after starting game

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,11 +1,21 @@
 var startButton = document.querySelector(".startButton");
-var timerNode = document.querySelector("#timer")
+var timerNode = document.querySelector("#timer");
+var mainContNode = document.querySelector("#mainContent");
+var feedbackNode = document.querySelector("#feedback")
+
+// Creating button and br\ elements in DOM
+var ansOptButton1 = document.createElement ("button");
+var ansOptButton2 = document.createElement ("button");
+var ansOptButton3 = document.createElement ("button");
+var ansOptButton4 = document.createElement ("button");
+
 
 
 var isWin = false;
 var timer;
 var timerCount;
 
+// Individual game questions stored in Objects
 var question1 = {
     question: "Which of the following is the assignment operator?",
     choice1: ["=","full"],
@@ -39,21 +49,37 @@ var question4 = {
 };
 
 var question5 = {
-    question: "What is the correct syntax for pushing information to the console?",
+    question: "What is the correct syntax for pushing information/data to the console?",
     choice1: "console.push()",
     choice2: "console()",
     choice3: ["console.log()","full"],
     choice4: "log()",
 };
 
+// All questions stored into an array for easy access
+
+var gameQuestions = [question1, question2, question3, question4, question5];
+
+// // The init function is called when the page loads 
+// function init() {
+//     getWins();
+//     getlosses();
+//   }
+
+// // Calls init() so that it fires when page openes
+// init();
+
+// Function for initiating game questions and timer. Start of game
 function startGame () {
     isWin = false;
     timerCount = 5;
-
+    // Preventing start button from working while game is in progress
     startButton.disabled = true;
+    renderQuestBoard();
     startTimer();
 };
 
+//Timer function that also checks for win and lose conditions. Timer stops for both scenarios 
 function startTimer() {
     timer = setInterval(function(){
     timerCount--;
@@ -65,13 +91,24 @@ function startTimer() {
     },1000);
 }
 
+//Function for appending buttons for each question option. This goes in the .midPanel. 
+function renderQuestBoard () {
+    mainContNode.textContent = " ";
+    feedbackNode.textContent = " ";
+    
+    mainContNode.setAttribute("style", "display: block;")
+
+    mainContNode.appendChild(ansOptButton1);
+    mainContNode.appendChild(ansOptButton2);
+    mainContNode.appendChild(ansOptButton3);
+    mainContNode.appendChild(ansOptButton4);
+
+    renderQuestions();
+};
+
+// Function for populating questions and answer options.
+function renderQuestions () {
+    
+}
+
 startButton.addEventListener("click", startGame);
-
-// The init function is called when the page loads 
-function init() {
-    getWins();
-    getlosses();
-  }
-
-  // Calls init() so that it fires when page opened
-init();
